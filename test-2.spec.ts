c664743931cec7024c37edf60d34706986deb19b
Lawrence <mblaw73@gmail.com>
2024-10-17 22:42:09 +0100

    first commit

--- test-2.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test('todo someting', async ({page}) =>{
-    await page.goto ('https://google.co.uk');
-    await page.getByAltText('Google').click();
-
-});
